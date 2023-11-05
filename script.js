f0601b6cf915e0a035f2201ca9c7532932d5cf29
Estebang2001 <110405527+Estebang2001@users.noreply.github.com>
2023-11-05 17:00:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let i = 1
+while (i<=5){
+    if (i==1) {
+        console.log("¡Bienvenido, 1!")
+        
+        
+    } else if (i==2) {
+        console.log("¡Bienvenido, " + i + "! Te presento a 1")
+       
+    } else if (i==3) {
+        console.log("¡Bienvenido, " + i + "! Te presento a 1 y a 2")
+      
+    } else if (i==4) {
+        console.log("¡Bienvenido, " + i + "! Te presento a 1, 2 y a 3")
+     
+    } else if (i==5) {
+        console.log("¡Bienvenido, " + i + "! Te presento a 1, 2, 3 y a 4")
+        
+    } i++ 
+    } 
